a43ad95d1d4bcaaefa9a4abb703eaa07a4dbd420
pavelkpetkov <pavelpetkov@mail.bg>
2021-10-05 23:16:32 +0300

    profile page added

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const { getUserById }  = require('../services/user');
+const { getAllInits } = require('../services/initiative');
 
 router.get('/', (req, res) => {
     res.render('home/home');
@@ -6,6 +8,25 @@ router.get('/', (req, res) => {
 
 router.get('/about', (req, res) => {
     res.render('home/about');
+})
+
+router.get('/user', async (req, res) => {
+
+    const user = await getUserById(req.user._id);
+
+    const allInits = await getAllInits();
+
+    let numberOfInits = 0;
+    let inits = [];
+    for (const init of allInits) {
+        if(init.author == req.user._id) {
+            numberOfInits ++;
+            inits.push(init);
+        };
+    }
+
+    console.log(numberOfInits);
+    res.render('user/profile', { user, inits, numberOfInits });
 })
 
 module.exports = router;
